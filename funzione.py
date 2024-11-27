7df2c3f588d64955ad3afa7eed77d8bf4af87fdb
Marcovar <elvara@hotmail.it>
2024-11-27 20:22:37 +0100

    Add files via upload

---
+++ funzione.py
@@ -0,0 +1,8 @@
+def saluto_personalizzato(nome):
+    saluto = f"Il tuo nome è: {nome} !"
+    nome_modificato =(nome.upper())
+    return saluto, nome_modificato
+
+variabile_fuori_funzione = saluto_personalizzato("Marco")
+print(variabile_fuori_funzione)
+print(type(variabile_fuori_funzione))
